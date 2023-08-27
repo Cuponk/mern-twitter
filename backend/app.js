1c05d279e43a1371e49fdae89ad03bc35706e5be
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2023-08-27 15:51:42 -0700

    add error

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const debug = require("debug");
 
-const cors = require('cors');
-const { isProduction } = require('./config/keys');
-const csurf = require('csurf');
+const cors = require("cors");
+const { isProduction } = require("./config/keys");
+const csurf = require("csurf");
 
-var usersRouter = require('./routes/api/users');
-var tweetsRouter = require('./routes/api/tweets');
-const csrfRouter = require('./routes/api/csrf');
+var usersRouter = require("./routes/api/users");
+var tweetsRouter = require("./routes/api/tweets");
+const csrfRouter = require("./routes/api/csrf");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -27,13 +28,33 @@ app.use(
             secure: isProduction,
             sameSite: isProduction && "Lax",
             httpOnly: true,
-        }
+        },
     })
 );
 
+app.use("/api/users", usersRouter);
+app.use("/api/tweets", tweetsRouter);
+app.use("/api/csrf", csrfRouter);
 
-app.use('/api/users', usersRouter);
-app.use('/api/tweets', tweetsRouter);
-app.use('/api/csrf', csrfRouter);
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug("backend:error");
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        statusCode,
+        errors: err.errors,
+    });
+});
 
 module.exports = app;
